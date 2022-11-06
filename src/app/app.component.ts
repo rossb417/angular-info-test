a71c69f5c3401b00f37e8737181b1871a53b5821
rossb417 <rossb417@users.noreply.github.com>
2022-11-05 23:16:37 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Resume } from './app.model';
+import { Clients, Portfolio, Resume, Skillset } from './app.model';
 
 import { PrimeIcons } from 'primeng/api';
 
@@ -12,6 +12,9 @@ export class AppComponent {
   public model = class {
     public static showInfo: boolean;
     public static resumeInfo = Resume;
+    public static skillset = Skillset.skillset;
+    public static clients = Clients.clients;
+    public static portfolio = Portfolio.portfolio;
     public static display: boolean;
   };
 
@@ -19,50 +22,7 @@ export class AppComponent {
     this.model.display = true;
   }
 
-  events1: any[];
-
-  events2: any[];
-
   ngOnInit() {
-    this.events1 = [
-      {
-  
-        date: '2017 +',
-        icon: PrimeIcons.SHOPPING_CART,
-        color: '#9C27B0',
-        company: 'Sqad',
-        position: 'UI Designer/Developer',
-        summary:
-          'UI designer and developer who was responsible for refactoring the design of new application. Utilizing Figma/XD for wireframe concepts, and would pitch features weekly. Wrote our own styles guides and component libraries for an enterprise level application. Many features integrated, but implemented dark, light, and black mode design for this app. The design has been through many refactors and implemented so many features for this application',
-        img_thumb: 'https://rs-brown.com/images/thumbs/fender-thumb.jpg',
-        img: 'https://rs-brown.com/images/info/fender/fender-3.jpg',
-      },
-      {
-  
-        date: '2014-2015',
-        icon: PrimeIcons.SHOPPING_CART,
-        color: '#9C27B0',
-        company: 'Fender',
-        position: 'Sr Interactive Designer',
-        summary:
-          'Designed Fender owned websites, applications, and internal feature pages, often with very tight deadlines. My work also included the biggest migration of all six Fender sites to an all-responsive framework. All sites were responsive designs utilizing mobile first approach. Tools used for the design side UI were Photoshop/XD',
-        img_thumb: 'https://rs-brown.com/images/thumbs/fender-thumb.jpg',
-        img: 'https://rs-brown.com/images/info/fender/fender-3.jpg',
-      },
-      {
-        status: 'Processing',
-        date: '2012-2014',
-        icon: PrimeIcons.COG,
-        color: '#673AB7',
-        company: 'SheKnows',
-        position: 'Sr. UI/UX Design/Developer',
-        summary:
-          'Originally hired for the design team and then went to lead the UX team. Designed feature sites for SheKnows.com owned websites. Designed custom newsletters and woman magazines that were converted to online blogs. Designed Facebook site for ones of the greatest photographers in the world',
-        img_thumb: 'https://rs-brown.com/images/thumbs/samsonite-thumb.jpg',
-        img: 'https://rs-brown.com/images/info/fender/samsonite-2.jpg',
-      },
- 
-    ];
-
+   // this.model.portfolio = Portfolio.portfolio;
   }
 }
--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -1,4 +1,120 @@
 import { Injectable } from '@angular/core';
+import { PrimeIcons } from 'primeng/api';
+
+export const Skillset = {
+  skillset: [
+    {
+      class: 'skillset',
+      header: '',
+      columns: [
+        {
+          heading: 'Tools/Software',
+          data: [
+            { line: 'Visual Studio, Eclipse' },
+            { line: 'balsamiq, Axure, Figma' },
+            { line: 'Github, GitBucket, Tower' },
+            { line: 'Photoshop, Illustrator, After Effects' },
+          ],
+        },
+        {
+          heading: 'Frameworks/CMS',
+          data: [
+            { line: 'Angular 7+, React, Vue' },
+            { line: 'Demandware, Shopify, Amazon, Volusion' },
+            { line: 'Wordpress , Joomla, EE' },
+            { line: 'Cake PHP/Smarty 3, Codeigniter' },
+          ],
+        },
+
+        {
+          heading: 'Libraries',
+          data: [
+            { line: 'LESS / SASS' },
+            { line: 'Primeflex' },
+            { line: 'Bootstrap / Foundation' },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export const Portfolio = {
+  portfolio: [
+    {
+
+      date: '2017 +',
+      icon: PrimeIcons.SHOPPING_CART,
+      color: '#9C27B0',
+      company: 'Sqad',
+      position: 'UI Designer/Developer',
+      summary:
+        'UI designer and developer who was responsible for refactoring the design of new application. Utilizing Figma/XD for wireframe concepts, and would pitch features weekly. Wrote our own styles guides and component libraries for an enterprise level application. Many features integrated, but implemented dark, light, and black mode design for this app. The design has been through many refactors and implemented so many features for this application',
+      img_thumb: 'https://rs-brown.com/images/thumbs/fender-thumb.jpg',
+      img: 'https://rs-brown.com/images/info/fender/fender-3.jpg',
+    },
+    {
+
+      date: '2014-2015',
+      icon: PrimeIcons.SHOPPING_CART,
+      color: '#9C27B0',
+      company: 'Fender',
+      position: 'Sr Interactive Designer',
+      summary:
+        'Designed Fender owned websites, applications, and internal feature pages, often with very tight deadlines. My work also included the biggest migration of all six Fender sites to an all-responsive framework. All sites were responsive designs utilizing mobile first approach. Tools used for the design side UI were Photoshop/XD',
+      img_thumb: 'https://rs-brown.com/images/thumbs/fender-thumb.jpg',
+      img: 'https://rs-brown.com/images/info/fender/fender-3.jpg',
+    },
+    {
+      status: 'Processing',
+      date: '2012-2014',
+      icon: PrimeIcons.COG,
+      color: '#673AB7',
+      company: 'SheKnows',
+      position: 'Sr. UI/UX Design/Developer',
+      summary:
+        'Originally hired for the design team and then went to lead the UX team. Designed feature sites for SheKnows.com owned websites. Designed custom newsletters and woman magazines that were converted to online blogs. Designed Facebook site for ones of the greatest photographers in the world',
+      img_thumb: 'https://rs-brown.com/images/thumbs/samsonite-thumb.jpg',
+      img: 'https://rs-brown.com/images/info/fender/samsonite-2.jpg',
+    },
+
+  ],
+};
+
+export const Clients = {
+  clients: [
+    {
+      class: 'clients',
+      header: 'Clients I have worked for',
+      images: [
+        {
+          img_thumb: 'https://rs-brown.com/images/clients/thumbs/fender.png',
+        },
+        {
+          img_thumb: 'https://rs-brown.com/images/clients/thumbs/motorola.png',
+        },
+        {
+          img_thumb: 'https://rs-brown.com/images/clients/thumbs/sheknows.png',
+        },
+        {
+          img_thumb: 'https://rs-brown.com/images/clients/thumbs/bo.png',
+        },
+        {
+          img_thumb: 'https://rs-brown.com/images/clients/thumbs/jackson.png',
+        },
+        {
+          img_thumb: 'https://rs-brown.com/images/clients/thumbs/mtv.png',
+        },
+        {
+          img_thumb: 'https://rs-brown.com/images/clients/thumbs/samsonite.png',
+        },
+        {
+          img_thumb: 'https://rs-brown.com/images/clients/thumbs/parkn.png',
+        },
+      ],
+    },
+  ],
+};
 
 export const Resume = {
   clients: [
@@ -40,7 +156,6 @@ export const Resume = {
       header: '',
       data: [
         {
-     
           company: 'Fender',
           position: 'Sr Interactive Designer',
           summary:
@@ -49,7 +164,6 @@ export const Resume = {
           img: 'https://rs-brown.com/images/info/fender/fender-3.jpg',
         },
         {
-      
           company: 'Samsonite Store 2012',
           position: 'UI/UX Design',
           summary:
