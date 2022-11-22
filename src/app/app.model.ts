10dc178ebb59e50deb76261725a754d71b36d608
rossb417 <rossb417@users.noreply.github.com>
2022-11-21 19:34:53 -0800

    2 files modified

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -44,7 +44,7 @@ export const Portfolio = [
       icon: PrimeIcons.SHOPPING_CART,
       color: '#9C27B0',
       company: 'Sqad',
-      position: 'UI Designer/Developer',
+      position: 'UI Architect / UI Developer',
       summary:
         'UI designer and developer who was responsible for refactoring the design of new application. Utilizing Figma/XD for wireframe concepts, and would pitch features weekly. Wrote our own styles guides and component libraries for an enterprise level application. Many features integrated, but implemented dark, light, and black mode design for this app. The design has been through many refactors and implemented so many features for this application',
       img_thumb: 'https://rs-brown.com/images/thumbs/fender-thumb.jpg',
@@ -56,7 +56,7 @@ export const Portfolio = [
       icon: PrimeIcons.SHOPPING_CART,
       color: '#9C27B0',
       company: 'Fender',
-      position: 'Sr Interactive Designer',
+      position: 'Sr Interactive Designer / Sr Front End Developer',
       summary:
         'Designed Fender owned websites, applications, and internal feature pages, often with very tight deadlines. My work also included the biggest migration of all six Fender sites to an all-responsive framework. All sites were responsive designs utilizing mobile first approach. Tools used for the design side UI were Photoshop/XD',
       img_thumb: 'https://rs-brown.com/images/thumbs/fender-thumb.jpg',
@@ -68,7 +68,7 @@ export const Portfolio = [
       icon: PrimeIcons.COG,
       color: '#673AB7',
       company: 'SheKnows',
-      position: 'Sr. UI/UX Design/Developer',
+      position: 'Sr. UI/UX Designer / Software Engineer',
       summary:
         'Originally hired for the design team and then went to lead the UX team. Designed feature sites for SheKnows.com owned websites. Designed custom newsletters and woman magazines that were converted to online blogs. Designed Facebook site for ones of the greatest photographers in the world',
       img_thumb: 'https://rs-brown.com/images/thumbs/samsonite-thumb.jpg',
